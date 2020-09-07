300c75fc646d780cf0ecbf3b1ee0f4ef8ea9d9bf
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-09-07 09:21:35 -0500

    Update python config

--- .zsh_etc/python.zsh
+++ .zsh_etc/python.zsh
@@ -8,3 +8,9 @@ export PATH="$HOME/Library/Python/3.7/bin:$PATH"
 
 alias python="python3"
 alias pip="pip3"
+
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+export PROJECT_HOME=$HOME/Code
+source /usr/local/bin/virtualenvwrapper.sh
